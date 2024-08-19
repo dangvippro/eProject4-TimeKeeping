a5f799585edfde7eefa2cc3664236aadd8067409
Đăng Vip Pro <ntdanga22032@cusc.ctu.edu.vn>
2024-08-19 16:39:14 +0700

    demo giao dien client

--- src/main/java/com/timekeeping/timekeeping/controllers/HomeController.java
+++ src/main/java/com/timekeeping/timekeeping/controllers/HomeController.java
@@ -11,6 +11,31 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home(Model model) {
-        return "/home/home";
+        model.addAttribute("title", "Home Page");
+        return "home/index";
+    }
+    
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About");
+        return "home/about";
+    }
+    
+    @GetMapping("/service")
+    public String service(Model model) {
+        model.addAttribute("title", "Service");
+        return "home/services";
+    }
+    
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("title", "Blog");
+        return "home/blog";
+    }
+    
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("title", "Contact");
+        return "home/contact";
     }
 }
